7d8690e33d87ffcb6520ea78993153f4563c19aa
Brian Nguyen <heybriannguyen@gmail.com>
2022-03-14 00:18:40 -0700

    Initial commit

---
+++ typepad_to_jekyll.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import sys
+
+
+# Makes markdown files for each post in the import file
+def make_posts(import_file):
+    # Flag to write post's body
+    canWrite = False
+    f_name = ""
+    f_matter = {
+        "AUTHOR": "",
+        "TITLE": "",
+        "STATUS": "",
+        "DATE": "",
+        "BASENAME": "",
+    }
+    categories = []
+    post = """"""
+
+    with open(import_file, "r") as f_r:
+        for line in f_r:
+            if line.startswith(tuple(f_matter)):
+                f_matter[line.split(": ")[0]] = line.split(": ")[1].strip("\n")
+            elif line.startswith("CATEGORY:"):
+                categories.append(line.split("CATEGORY: ")[1].strip("\n"))
+            elif line.startswith("BODY:"):
+                f_name = write_f_name(f_matter)
+                write_f_matter(f_name, f_matter, categories)
+                canWrite = True
+            elif line == "-----\n":
+                # Skip ----- line at the EOF
+                continue
+            elif line.startswith("EXTENDED BODY:"):
+                canWrite = False
+                write_body(post, f_name)
+                # Prepares loop for new post and its categories
+                post = """"""
+                categories.clear()
+            elif canWrite:
+                post += line
+
+
+# Writes the filename
+def write_f_name(f_matter):
+    return (
+        f"""{f_matter['DATE'][6:10]}"""
+        + f"""-{f_matter['DATE'][:2]}"""  # Year
+        + f"""-{f_matter['DATE'][3:5]}"""  # Month
+        + f"""-{f_matter['BASENAME']}.md"""  # Day  # Filename
+    )
+
+
+# Writes at the top of the Markdown file
+def write_f_matter(f_name, f_matter, categories):
+    with open(f_name, "w") as f_w:
+        f_w.write(
+            f"""---\n"""
+            + f"""layout: post\n"""
+            + f"""author: {f_matter['AUTHOR']}\n"""
+            + f"""title: {f_matter['TITLE']}\n"""
+            + f"""status: {f_matter['STATUS']}\n"""
+            + f"""categories: {categories}\n"""
+            + f"""date: {f_name[:10]}\n"""
+            + f"""---\n\n"""
+        )
+
+
+# Writes the post
+def write_body(post, f_name):
+    with open(f_name, "a") as f_w:
+        f_w.write(post)
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+    make_posts(str(sys.argv[1]))
+    print(datetime.now() - start_time)
